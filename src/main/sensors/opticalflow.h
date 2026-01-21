f596572309b8bc62ce8aaecb52eb9672964aa64c
braeden-df <braeden@droneforge.us>
2026-01-20 20:11:41 -0500

    Always set MTF sensor to use Micolink protocol (#5)
    * optical driver maybe
    
    * mtf range
    
    * optflow sensor, maybe
    
    * maybe range
    
    * range woohoo
    
    * get of working
    
    * lpf on vel
    
    * better filtering for of
    
    * even more smooth of lpf
    
    * some optrange niceties
    
    * set OF task rate to official sensor refresh rate
    
    * set mtf to micolink protocol on task init
    
    * cleanup
    
    ---------
    
    Co-authored-by: Braeden Hall <braeden@brhall.dev>
    (cherry picked from commit 74ffd214b755cc51f0af6b28e7923ca32b6b2219)

--- src/main/sensors/opticalflow.h
+++ src/main/sensors/opticalflow.h
@@ -2,31 +2,31 @@
 
 #include <stdint.h>
 
-#include "drivers/optrange/optrange.h"
 #include "common/time.h"
+#include "drivers/optrange/optrange.h"
 #include "pg/pg.h"
 
 typedef enum {
-    OPTICALFLOW_NONE    = 0,
-    OPTICALFLOW_MTF02   = 1,
+  OPTICALFLOW_NONE = 0,
+  OPTICALFLOW_MTF02 = 1,
 } opticalflowType_e;
 
 typedef struct opticalflowConfig_s {
-    uint8_t opticalflow_hardware;
+  uint8_t opticalflow_hardware;
 } opticalflowConfig_t;
 
 PG_DECLARE(opticalflowConfig_t, opticalflowConfig);
 
 typedef struct opticalflow_s {
-    optrangeDev_t dev;
-    int16_t velX; // cm/s @ 1m
-    int16_t velY; // cm/s @ 1m
+  optrangeDev_t dev;
+  int16_t velX; // cm/s @ 1m
+  int16_t velY; // cm/s @ 1m
 #ifdef USE_RANGEFINDER_OPTFLOW_MTF
-    uint8_t flowQuality; // 0-255, higher is better
-    uint8_t flowStatus; // 0 is invalid, 1 is valid
+  uint8_t flowQuality; // 0-255, higher is better
+  uint8_t flowStatus;  // 0 is invalid, 1 is valid
 #endif
 
-    timeMs_t lastValidResponseTimeMs;
+  timeMs_t lastValidResponseTimeMs;
 } opticalflow_t;
 
 bool opticalflowInit(void);

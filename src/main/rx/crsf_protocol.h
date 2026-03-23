bee00f09e559c01f881a3677eb67cf28a814a592
Braeden Hall <braeden@brhall.dev>
2026-03-23 12:03:30 -0700

    add state frame to crsf
    (cherry picked from commit f16ae9691cfc5a2dfee8ef1319352366d91623c2)

--- src/main/rx/crsf_protocol.h
+++ src/main/rx/crsf_protocol.h
@@ -64,6 +64,7 @@ typedef enum {
     CRSF_FRAMETYPE_RAW_IMU = 0xD1, // raw imu data
     CRSF_FRAMETYPE_MOTOR_RPM = 0xD2, // motor output and RPM data
     CRSF_FRAMETYPE_OPTRANGE = 0xD3, // opticalflow rangefinder data
+    CRSF_FRAMETYPE_KINEMATIC_STATE = 0xD4, // EKF position/velocity + attitude data
 } crsfFrameType_e;
 
 enum {
@@ -107,6 +108,7 @@ enum {
     CRSF_FRAME_RANGEFINDER_TF_PAYLOAD_SIZE = 4,
     CRSF_FRAME_OPTRANGE_PAYLOAD_SIZE = 10,
     CRSF_FRAME_RAW_IMU_PAYLOAD_SIZE = 12,
+    CRSF_FRAME_KINEMATIC_STATE_PAYLOAD_SIZE = 18, // pos xyz, vel xyz, attitude rpy
 #ifdef USE_DSHOT_TELEMETRY
     CRSF_FRAME_MOTOR_RPM_PAYLOAD_SIZE = 13, // with telemetry: 1 pole + 4*(1 pwm + 2 erpm)
 #else
@@ -148,4 +150,3 @@ typedef enum {
     CRSF_ADDRESS_CRSF_RECEIVER = 0xEC,
     CRSF_ADDRESS_CRSF_TRANSMITTER = 0xEE
 } crsfAddress_e;
-
